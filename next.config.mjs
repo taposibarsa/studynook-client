5f4fe4e760b2f637a912a3d113892617ae8f8992
taposi barsa <taposibarsa@gmail.com>
2026-05-19 20:11:25 +0600

    image whitelist

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "suncart-store-ebon.vercel.app", pathname: "/**" },
+      { protocol: "https", hostname: "*.vercel.app", pathname: "/**" },
+      { protocol: "https", hostname: "i.ibb.co", pathname: "/**" },
+      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      { protocol: "https", hostname: "res.cloudinary.com", pathname: "/**" },
+      { protocol: "https", hostname: "picsum.photos", pathname: "/**" },
+      { protocol: "https", hostname: "placehold.co", pathname: "/**" },
+      { protocol: "https", hostname: "**.amazonaws.com", pathname: "/**" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com", pathname: "/**" },
+    ],
+  },
 };
 
 export default nextConfig;
